f3bba529127e811d6019fdcc554fcc79fd2c43df
Instituto Precisa Ser <joyceassis96@gmail.com>
2023-04-12 15:20:16 -0300

    code useMemo

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, {useState, useMemo} from 'react';
+
+export default function App(){
+const [tarefas, setTarefas] = useState([])
+const [input, setInput] = useState([])
+const [count, setCount] = useState(0)
+const aleatorio = useMemo(()=> count * Math.random(), [count])
+
+function handleAdd(){
+  setTarefas([...tarefas, input])
+}
+// //tarefas [estudar]
+// //tarefas [correr]
+// tarefas: [[estudar],[correr]]
+// //tarefas: [  estudar, correr]
+
+//const valor = useMemo(()=>{return código}, [dependencia])
+
+const totalTarefas = useMemo(()=> tarefas.length, [tarefas]) 
+  return(
+    <>
+    <input type="text" onChange={(e)=> setInput(e.target.value)} />
+    <button onClick={handleAdd} >Adicionar</button>
+    {tarefas.map((tarefas)=>(
+      <li>{tarefas}</li>
+    ))}
+    <h2>Você tem {totalTarefas} tarefas para fazer</h2>
+
+    <h2>{count}</h2>
+    <button onClick={()=>{ setCount(count +1)}}>+1</button>
+    <h3>multiplicar count por um numero aleatório: {aleatorio}</h3>
+    </>
+  )
+}
